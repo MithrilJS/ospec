72d9f07c977e6646d6dfc4b5ecfb1c52d866efdf
Kesara Rathnayake <kesara@fq.nz>
2019-10-20 21:27:59 +1300

    * Fix tests script. * Add mithril as test dependency.

--- tests/test-ospec.js
+++ tests/test-ospec.js
@@ -5,7 +5,7 @@ var o, callAsync
 
 if (typeof require !== "undefined") {
 	/* eslint-disable global-require */
-	callAsync = require("../../test-utils/callAsync")
+	callAsync = require("mithril/test-utils/callAsync")
 	o = require("../ospec")
 	/* eslint-enable global-require */
 } else {

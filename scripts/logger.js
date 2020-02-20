2dc9ba882e2dabd9bfbe1c5561f12ad8155901f3
Pierre-Yves Gérardy <py@py.gy>
2020-02-21 00:10:03 +0100

    Add a logger script to help debug complex issues

---
+++ scripts/logger.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const o = require("ospec")
+const {report} = o
+const {writeFileSync, mkdirSync} = require("fs")
+const {join} = require("path")
+
+console.log("Logging...")
+mkdirSync("./logs", {recursive: true})
+
+function toPlain({message, stack}) {
+	return {message, stack}
+}
+
+o.report = function(results) {
+	const path = join(".", "logs", `${results.length}-${String(Date.now())}.json`)
+	writeFileSync(path, JSON.stringify(results.map(
+		(r) => {
+			r.error=toPlain(r.error)
+			r.testError = toPlain(r.testError)
+			return r
+		}
+	), null, 2))
+	console.log(`results written to ${path}`)
+	return report(results)
+}
